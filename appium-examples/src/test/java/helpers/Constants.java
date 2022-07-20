b3939f13c5130a968089764624ebb4d0e85104c5
prasauce <109624817+prasauce@users.noreply.github.com>
2022-07-20 14:02:15 -0400

    updated region to US

--- appium-examples/src/test/java/helpers/Constants.java
+++ appium-examples/src/test/java/helpers/Constants.java
@@ -1,7 +1,7 @@
 package helpers;
 
 public class Constants {
-    public static final String region = System.getProperty("region", "eu");
+    public static final String region = System.getProperty("region", "us");
     public static final String host = System.getProperty("host", "saucelabs");
     public static final String rdc = System.getProperty("rdc", "true");
 
