b4f7acf3b73a518685a1558d412fdb9b864c1ba6
prasauce <109624817+prasauce@users.noreply.github.com>
2022-11-14 16:46:43 -0500

    Added a test that doesn't send pass/fail status

--- selenium-examples/src/test/java/com/saucedemo/selenium/login/SeleniumLoginTest.java
+++ selenium-examples/src/test/java/com/saucedemo/selenium/login/SeleniumLoginTest.java
@@ -112,6 +112,31 @@ public class SeleniumLoginTest {
 
         Assertions.assertEquals("https://www.saucedemo.com/inventory.html", driver.getCurrentUrl());
     }
+    
+    @DisplayName("Swag Labs Login with Selenium (Forgetting to Send Test Status)")
+    @RepeatedTest(2)
+    public void swagLabsLoginErrorTest() {
+        driver.executeScript("sauce:job-name=Swag Labs Login with Selenium (Forgetting to Send Test Status)");
+        driver.get("https://www.saucedemo.com");
+
+        By usernameFieldLocator = By.cssSelector("#user-name-wrong");
+        By passwordFieldLocator = By.cssSelector("#password");
+        By submitButtonLocator = By.cssSelector(".btn_action");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(180));
+        wait.until((driver) -> driver.findElement(usernameFieldLocator).isDisplayed());
+
+        WebElement userNameField = driver.findElement(usernameFieldLocator);
+        WebElement passwordField = driver.findElement(passwordFieldLocator);
+        WebElement submitButton = driver.findElement(submitButtonLocator);
+
+        userNameField.sendKeys("locked_out_user");
+        passwordField.sendKeys("secret_sauce");
+        submitButton.click();
+
+        Assertions.assertEquals("https://www.saucedemo.com/inventory.html", driver.getCurrentUrl());
+        driver.quit();
+    }
 
     /**
      * Custom TestWatcher for Sauce Labs projects.
